8524f05eaa98605c951664603b0278e58f4238b9
Chris Anstey <git@cpanstey.co.uk>
2019-12-21 15:01:15 +0000

    Create empty package

---
+++ src/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace Maraful\QuerySummary;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
