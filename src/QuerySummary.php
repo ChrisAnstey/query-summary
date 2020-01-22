a5004208e188aa200ed44b36eaa61fd4f0b9d1fc
Chris Anstey <git@cpanstey.co.uk>
2020-01-22 22:33:26 +0000

    Simplify group handling code
    No point grouping them, then mapping the data down to the raw counts, then going through again, and finding all the queries matching each unique query... Just group, then loop through each unique one.

--- src/QuerySummary.php
+++ src/QuerySummary.php
@@ -28,25 +28,18 @@ class QuerySummary extends QueryCollector implements DataCollectorInterface, Ren
     {
         $statements = collect($this->queries);
 
-        // produce a collection of unique queries, with the count of how many times each occurred
-        $counted = $statements->groupBy('query')->map(function ($items) {
-            return count($items);
-        });
+        // group them by query
+        $grouped = $statements->groupBy('query');
 
         // loop through the unique queries, producing the output data (total calls, query time etc)
-        $counted = $counted->map(function ($countedVal, $countedKey) use ($statements) {
-
-            // get a full collection of the queries that match the current unique one
-            $filtered = $statements->filter(function ($value) use ($countedKey) {
-                return $value['query'] == $countedKey;
-            });
+        $counted = $grouped->map(function ($group, $key) {
 
             // use the first one, and add the summary details
-            $firstExample = $filtered->first();
-            $firstExample['sql'] = sprintf('%sx ', $countedVal) . $firstExample['query'];
-            $firstExample['duration_str'] = $this->formatDuration($filtered->sum('time'));
-            $firstExample['duration'] = $filtered->sum('time');
-            $firstExample['count'] = $countedVal;
+            $firstExample = $group->first();
+            $firstExample['sql'] = sprintf('%sx ', $group->count()) . $firstExample['query'];
+            $firstExample['duration_str'] = $this->formatDuration($group->sum('time'));
+            $firstExample['duration'] = $group->sum('time');
+            $firstExample['count'] = $group->count();
             $firstExample['backtrace'] = array_values($firstExample['source']);
             $firstExample['stmt_id'] = $this->getDataFormatter()->formatSource(reset($firstExample['source']));
 
